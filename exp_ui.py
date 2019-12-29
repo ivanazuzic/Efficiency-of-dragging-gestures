ca59b9f8d39437e93f0446f8ae0c64966a151bca
Mateja Napravnik <44111384+mnapravnik@users.noreply.github.com>
2019-12-29 19:00:19 +0100

    Added basic point drawing when mouse is clicked

--- exp_ui.py
+++ exp_ui.py
@@ -10,6 +10,8 @@ import numpy as np
 
 ##################### function init #####################
 
+SAMPLE_TIMEOUT = 10  # how much milliseconds pass between sampling
+
 x_range = {
     # start point and end point on which f(x) is defined
     "start": 0,
@@ -55,7 +57,8 @@ for i in range(len(y)):
     y[i] = function_curve(t[i])  # calculate y for each generated t
 
 
-fig.add_subplot(111).plot(t, y)  # plot the generated t and y
+graph = fig.add_subplot(111)  # this is the subplot on which we draw
+graph.plot(t, y)  # plot the generated t and y
 
 canvas = FigureCanvasTkAgg(fig, master=root)  # A tk.DrawingArea.
 canvas.draw()
@@ -65,22 +68,24 @@ toolbar = NavigationToolbar2Tk(canvas, root)
 toolbar.update()
 canvas.get_tk_widget().pack(side=tkinter.TOP, fill=tkinter.BOTH, expand=1)
 
-mouse_is_pressed = False
-# cursor_coord is where last logged mouse coordinates will be stored,
+is_mouse_pressed = False
+# cursor_coord holds the last (ultimate) logged cursor coordinate,
 # is (0, 0) by default
+cursor_coord = {"x": 0, "y": 0}
+
 cursor_coord = {"x": 0, "y": 0}
 
 
 def on_mouse_down(event):
-    global mouse_is_pressed
+    global is_mouse_pressed
     print("you clicked")
-    mouse_is_pressed = True
+    is_mouse_pressed = True
 
 
 def on_mouse_up(event):
-    global mouse_is_pressed
+    global is_mouse_pressed
     print("you released")
-    mouse_is_pressed = False
+    is_mouse_pressed = False
 
 
 def on_mouse_hover(event):
@@ -95,15 +100,31 @@ canvas.mpl_connect("motion_notify_event", on_mouse_hover)
 
 
 def task():
-    if mouse_is_pressed:
-        print(cursor_coord)
+    global graph, fig
+    if is_mouse_pressed:
         # TODO:
         # if mouse is pressed, check where the cursor lies,
         # check if it's near the example function
         # and then do something smart with that information.
+
+        # plotting a point on the subplot
+        # at the current cursor coordinates (x, y).
+        graph.scatter(
+            cursor_coord["x"],
+            cursor_coord["y"],
+            # make it wider than the original curve
+            linewidth=2,
+            # give it a subtle color to avoid visual clutter
+            # and make it 80% visible
+            color=(0.7, 0.8, 0.7, 0.8)
+        )
+        fig.canvas.draw()  # update the canvas
+
     else:
-        print("no")
-    root.after(1000, task)
+        # do something if mouse is not pressed...
+        pass
+
+    root.after(SAMPLE_TIMEOUT, task)
 
 
 def _quit():
@@ -115,7 +136,7 @@ def _quit():
 button = tkinter.Button(master=root, text="Quit", command=_quit)
 button.pack(side=tkinter.BOTTOM)
 
-root.after(1000, task)
+root.after(SAMPLE_TIMEOUT, task)
 tkinter.mainloop()
 # If you put root.destroy() here, it will cause an error if the window is
 # closed with the window manager.
