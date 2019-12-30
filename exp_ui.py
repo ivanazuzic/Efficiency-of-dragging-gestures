1a583441c05ddafca4fb40edbb4a580b9ed168a7
Ivana Zuzic <izuzic3@gmail.com>
2019-12-30 16:10:54 +0100

    Created classes for the main and the experiment window The main window class takes the participants data. The data isn't sent to the Google sheet yet, so the parts of code regarding this are commented. The experiment window class holds the graph and detects clicks on it. Probably the graph should be separated as its own class in the future.

--- exp_ui.py
+++ exp_ui.py
@@ -1,4 +1,4 @@
-import tkinter
+import tkinter as tk
 
 from matplotlib.backends.backend_tkagg import (
     FigureCanvasTkAgg, NavigationToolbar2Tk)
@@ -8,135 +8,123 @@ from matplotlib.figure import Figure
 
 import numpy as np
 
-##################### function init #####################
+class ExperimentWindow(tk.Frame):
+    def __init__(self, parent, *args, **kwargs):
+        tk.Frame.__init__(self, parent, *args, **kwargs)
+        self.parent = parent
 
-SAMPLE_TIMEOUT = 10  # how much milliseconds pass between sampling
+        self.SAMPLE_TIMEOUT = 10  # how much milliseconds pass between sampling
 
-x_range = {
-    # start point and end point on which f(x) is defined
-    "start": 0,
-    "end": 50,
-}
+        self.x_range = {
+            # start point and end point on which f(x) is defined
+            "start": 0,
+            "end": 50,
+        }
 
+        self.window = tk.Toplevel()
+        self.window.title("Task window")
 
-def function_curve(x):
-    # this is the function we're plotting.
-    # it returns its value y=f(x) based on the value of the
-    # given x.
+        self.fig = Figure(figsize=(5, 4), dpi=100)
 
-    # this example function has two parts,
-    # equally divided across x_range.
-
-    diff = np.abs(x_range["end"] - x_range["start"])
-    divide_point = (diff / 2.0) + x_range["start"]
-
-    if(x > divide_point):
-        return 5 * np.sin(x)
-    else:
-        return x * x - 15 * x
-
-
-##########################################
-
-
-root = tkinter.Tk()
-root.wm_title("Embedding in Tk")
-
-fig = Figure(figsize=(5, 4), dpi=100)
-
-# the following line always produces 100 equally spread points on the x_range
-t = np.arange(
-    x_range["start"],
-    x_range["end"],
-    np.abs(x_range["end"] - x_range["start"]) / 100.00
-)
-
-y = np.zeros(len(t))
-
-for i in range(len(y)):
-    y[i] = function_curve(t[i])  # calculate y for each generated t
-
-
-graph = fig.add_subplot(111)  # this is the subplot on which we draw
-graph.plot(t, y)  # plot the generated t and y
-
-canvas = FigureCanvasTkAgg(fig, master=root)  # A tk.DrawingArea.
-canvas.draw()
-canvas.get_tk_widget().pack(side=tkinter.TOP, fill=tkinter.BOTH, expand=1)
-
-toolbar = NavigationToolbar2Tk(canvas, root)
-toolbar.update()
-canvas.get_tk_widget().pack(side=tkinter.TOP, fill=tkinter.BOTH, expand=1)
-
-is_mouse_pressed = False
-# cursor_coord holds the last (ultimate) logged cursor coordinate,
-# is (0, 0) by default
-cursor_coord = {"x": 0, "y": 0}
-
-cursor_coord = {"x": 0, "y": 0}
-
-
-def on_mouse_down(event):
-    global is_mouse_pressed
-    print("you clicked")
-    is_mouse_pressed = True
-
-
-def on_mouse_up(event):
-    global is_mouse_pressed
-    print("you released")
-    is_mouse_pressed = False
-
-
-def on_mouse_hover(event):
-    # this frequently updates the cursor location
-    cursor_coord["x"] = event.xdata
-    cursor_coord["y"] = event.ydata
-
-
-canvas.mpl_connect("button_press_event", on_mouse_down)
-canvas.mpl_connect("button_release_event", on_mouse_up)
-canvas.mpl_connect("motion_notify_event", on_mouse_hover)
-
-
-def task():
-    global graph, fig
-    if is_mouse_pressed:
-        # TODO:
-        # if mouse is pressed, check where the cursor lies,
-        # check if it's near the example function
-        # and then do something smart with that information.
-
-        # plotting a point on the subplot
-        # at the current cursor coordinates (x, y).
-        graph.scatter(
-            cursor_coord["x"],
-            cursor_coord["y"],
-            # make it wider than the original curve
-            linewidth=2,
-            # give it a subtle color to avoid visual clutter
-            # and make it 80% visible
-            color=(0.7, 0.8, 0.7, 0.8)
+        # the following line always produces 100 equally spread points on the x_range
+        self.t = np.arange(
+            self.x_range["start"],
+            self.x_range["end"],
+            np.abs(self.x_range["end"] - self.x_range["start"]) / 100.00
         )
-        fig.canvas.draw()  # update the canvas
 
-    else:
-        # do something if mouse is not pressed...
-        pass
+        self.y = np.zeros(len(self.t))
+        self.y = self.function_curve()
 
-    root.after(SAMPLE_TIMEOUT, task)
+        self.graph = self.fig.add_subplot(111)  # this is the subplot on which we draw
+        self.graph.plot(self.t, self.y)  # plot the generated t and y
+
+        self.canvas = FigureCanvasTkAgg(self.fig, master=self.window)  # A tk.DrawingArea.
+        self.canvas.draw()
+        self.canvas.get_tk_widget().grid(row=0, column=0)
+
+        self.is_mouse_pressed = False
+        # cursor_coord holds the last (ultimate) logged cursor coordinate,
+        # is (0, 0) by default
+        self.cursor_coord = {"x": 0, "y": 0}
+
+        self.cursor_coord = {"x": 0, "y": 0}
+
+        self.canvas.mpl_connect("button_press_event", self.on_mouse_down)
+        self.canvas.mpl_connect("button_release_event", self.on_mouse_up)
+        self.canvas.mpl_connect("motion_notify_event", self.on_mouse_hover)
+
+        self.button = tk.Button(master=self.window, text="Quit", command=self._quit)
+        self.button.grid(row=2, column=0)
+
+        self.window.after(self.SAMPLE_TIMEOUT, self.task)
+
+    
+    def _quit(self):
+        self.window.quit()     # stops mainloop
+        self.window.destroy()  # this is necessary on Windows to prevent
+                        # Fatal Python Error: PyEval_RestoreThread: NULL tstate
+
+    def task(self):
+        if self.is_mouse_pressed:
+            # TODO:
+            # if mouse is pressed, check where the cursor lies,
+            # check if it's near the example function
+            # and then do something smart with that information.
+
+            # plotting a point on the subplot
+            # at the current cursor coordinates (x, y).
+            self.graph.scatter(
+                self.cursor_coord["x"],
+                self.cursor_coord["y"],
+                # make it wider than the original curve
+                linewidth=2,
+                # give it a subtle color to avoid visual clutter
+                # and make it 80% visible
+                color=(0.7, 0.8, 0.7, 0.8)
+            )
+            self.fig.canvas.draw()  # update the canvas
+
+        else:
+            # do something if mouse is not pressed...
+            pass
+
+        self.window.after(self.SAMPLE_TIMEOUT, self.task)
+
+    def function_curve(self):
+        # this is the function we're plotting.
+        # it returns its value y=f(x) based on the value of the
+        # given x.
+
+        # this example function has two parts,
+        # equally divided across x_range.
+
+        y = np.zeros(len(self.t))
+
+        for x in range(len(self.y)):
+            # calculate y for each generated t
+
+            diff = np.abs(self.x_range["end"] - self.x_range["start"])
+            divide_point = (diff / 2.0) + self.x_range["start"]
+
+            if(x > divide_point):
+                y[x] = 5 * np.sin(x)
+            else:
+                y[x] = x * x - 15 * x
+
+        return y
+
+    def on_mouse_down(self, event):
+        print("you clicked")
+        self.is_mouse_pressed = True
 
 
-def _quit():
-    root.quit()     # stops mainloop
-    root.destroy()  # this is necessary on Windows to prevent
-                    # Fatal Python Error: PyEval_RestoreThread: NULL tstate
+    def on_mouse_up(self, event):
+        print("you released")
+        self.is_mouse_pressed = False
 
 
-button = tkinter.Button(master=root, text="Quit", command=_quit)
-button.pack(side=tkinter.BOTTOM)
-
-root.after(SAMPLE_TIMEOUT, task)
-tkinter.mainloop()
-# If you put root.destroy() here, it will cause an error if the window is
-# closed with the window manager.
+    def on_mouse_hover(self, event):
+        # this frequently updates the cursor location
+        self.cursor_coord["x"] = event.xdata
+        self.cursor_coord["y"] = event.ydata
